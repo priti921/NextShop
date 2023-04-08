dda1a06cf3a29a887089ffbe711ca61ac193b5c4
Priti Roy <roypriti921@gmail.com>
2023-04-08 19:20:01 +0530

    added image normalizer to change image data for better usage

--- framework/shopify/utils/normalize.ts
+++ framework/shopify/utils/normalize.ts
@@ -1,12 +1,30 @@
 //normalizing product details aka changing their data handles
 
-import { Product as ShopifyProduct } from "../schema";
+import { ImageEdge, Product as ShopifyProduct } from "../schema";
 
+//normalizing product images   //param type specified as array type of ImageEdge
+function normalizeProductImage({ edges }: { edges: Array<ImageEdge> }) {
+  //mapping through edges => on each item in the array destructuring node => node then destructured into originalSrc(aliased to url) and the rest of properties
+  return edges.map(({ node: { originalSrc: url, ...rest } }) => {
+    return {
+      url: `/images/${url}`,
+      ...rest,
+    };
+  });
+}
+
+//param type specified
 export function normalizeProduct(productNode: ShopifyProduct) {
-  //param type specified
-
   //destructuring product
-  const { id, title: name, handle, vendor, description, ...rest } = productNode;
+  const {
+    id,
+    title: name,
+    handle,
+    vendor,
+    description,
+    images: ImageConnection,
+    ...rest
+  } = productNode;
 
   //reassigning product details
   const product = {
@@ -14,6 +32,7 @@ export function normalizeProduct(productNode: ShopifyProduct) {
     name,
     vendor,
     description,
+    image: normalizeProductImage(ImageConnection),
     path: `/${handle}`,
     slug: handle.replace(/^\/+|\/+$/g, ""), //replace with slug, remove leading and ending slashes
     ...rest,
