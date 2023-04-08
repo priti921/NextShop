83b06afaec7d0799128097b58dabc7dee12490f3
Priti Roy <roypriti921@gmail.com>
2023-04-08 19:31:34 +0530

    created product types and specified them as return type in normalizer function and getAllProducts function

--- framework/shopify/products/get-all-products.ts
+++ framework/shopify/products/get-all-products.ts
@@ -2,12 +2,13 @@ import getAllProductsQuery from "../utils/queries/get-all-products";
 import fetchAPI from "../utils/fetch-api";
 import { ProductConnection } from "../schema";
 import { normalizeProduct } from "../utils/normalize";
+import { Product } from "../../common/types/product";
 
 type ReturnType = {
   products: ProductConnection;
 };
 
-const getAllProducts = async (): Promise<any> => {
+const getAllProducts = async (): Promise<Product[]> => {
   const { data } = await fetchAPI<ReturnType>({ query: getAllProductsQuery }); //added fetchAPI function return type
 
   //mapping through edges to get products, destructured edge into { node: product} alias
--- framework/shopify/utils/normalize.ts
+++ framework/shopify/utils/normalize.ts
@@ -1,6 +1,6 @@
 //normalizing product details aka changing their data handles
-
 import { ImageEdge, Product as ShopifyProduct } from "../schema";
+import { Product } from "../../common/types/product";
 
 //normalizing product images   //param type specified as array type of ImageEdge
 const normalizeProductImage = ({ edges }: { edges: Array<ImageEdge> }) => {
@@ -14,7 +14,7 @@ const normalizeProductImage = ({ edges }: { edges: Array<ImageEdge> }) => {
 };
 
 //param type specified
-export function normalizeProduct(productNode: ShopifyProduct) {
+export function normalizeProduct(productNode: ShopifyProduct): Product {
   //destructuring product
   const {
     id,
@@ -32,7 +32,7 @@ export function normalizeProduct(productNode: ShopifyProduct) {
     name,
     vendor,
     description,
-    image: normalizeProductImage(ImageConnection),
+    images: normalizeProductImage(ImageConnection),
     path: `/${handle}`,
     slug: handle.replace(/^\/+|\/+$/g, ""), //replace with slug, remove leading and ending slashes
     ...rest,
