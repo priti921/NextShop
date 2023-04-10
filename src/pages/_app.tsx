9bd258288ff81254c4adc42db41a9ae31abb1772
Priti Roy <roypriti921@gmail.com>
2023-04-10 18:49:59 +0530

    added default exporting index files and configured tsconfig file import alias

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import Layout from "components/common/Layout/Layout";
+import { Layout } from "@components/common";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
