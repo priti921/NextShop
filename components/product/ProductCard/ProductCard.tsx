92abe2a6373294a17ae59887aab5ed53ddd22ed3
Priti Roy <roypriti921@gmail.com>
2023-04-10 23:08:59 +0530

    added component details wit ha demo image for testing

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -1,12 +1,37 @@
 import { Product } from "@common/types/product";
+import Image from "next/image";
+import Link from "next/link";
 import React, { FC } from "react";
 
 interface Props {
   product: Product;
 }
 
+import placeholderImage from "./placeholderImage.png";
+
 const ProductCard: FC<Props> = ({ product }) => {
-  return <div>{product.name}</div>;
+  return (
+    <Link href={`/products/${product.slug}`}>
+      <div>
+        <div>
+          <h3>
+            <span>{product.name}</span>
+          </h3>
+          <span>{product.description}</span>
+        </div>
+        {product.images && (
+          <Image
+            src={placeholderImage}
+            alt={product.name ?? "product image"}
+            height={540}
+            width={540}
+            quality="85"
+            layout="responsive"
+          />
+        )}
+      </div>
+    </Link>
+  );
 };
 
 export default ProductCard;
