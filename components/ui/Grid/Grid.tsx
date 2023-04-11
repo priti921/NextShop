dcc6f5246986867a5991f07a52273a1e27047d3f
Priti Roy <roypriti921@gmail.com>
2023-04-11 13:08:04 +0530

    testing conditional layout styles with classnames packege

--- components/ui/Grid/Grid.tsx
+++ components/ui/Grid/Grid.tsx
@@ -1,8 +1,19 @@
 import React, { FC, ReactNode } from "react";
 import style from "./Grid.module.css";
+import cn from "classnames";
 
-const Grid: FC<{ children: ReactNode }> = ({ children }) => {
-  return <div className={style.root}>{children}</div>;
+interface Props {
+  children: ReactNode[];
+  layout?: "A" | "B";
+}
+
+const Grid: FC<Props> = ({ children, layout = "A" }) => {
+  const rootClassName = cn(style.root, {
+    [style.layoutA]: layout === "A",
+    [style.layoutB]: layout === "B",
+  });
+
+  return <div className={rootClassName}>{children}</div>;
 };
 
 export default Grid;
