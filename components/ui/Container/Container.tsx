8b4776e5e79d104ff9755ea5de40f06153d5e1ff
Priti Roy <roypriti921@gmail.com>
2023-04-13 22:14:17 +0530

    instead of using inline styling extended tailconfig and added max with of 8xl

--- components/ui/Container/Container.tsx
+++ components/ui/Container/Container.tsx
@@ -1,11 +1,7 @@
 import React, { FC, ReactNode } from "react";
 
 const Container: FC<{ children: ReactNode | ReactNode[] }> = ({ children }) => {
-  return (
-    <div className="px-6 mx-auto" style={{ maxWidth: "1920px" }}>
-      {children}
-    </div>
-  );
+  return <div className="px-6 mx-auto max-w-8xl">{children}</div>;
 };
 
 export default Container;
