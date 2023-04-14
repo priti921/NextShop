b35fde0fd7939b5371b81eb1dddf9105d5026bf1
Priti Roy <roypriti921@gmail.com>
2023-04-14 16:24:49 +0530

    added navbar links and styling

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import React, { FC } from "react";
 const Footer: FC = () => {
   return (
     <footer className="bg-black pt-1">
-      <div className="container max-auto px-6">
+      <div className="container mx-auto px-6">
         <div className="mt-5 flex flex-col items-center">
           <div className="py-6">
             <p className="mb-b text-sm text-primary-2 font-bold">
--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -1,11 +1,29 @@
 import React, { FC } from "react";
 import { Container } from "@components/ui";
+import Link from "next/link";
+import style from "./Navbar.module.css";
 
 const Navbar: FC = () => {
   return (
     <Container>
-      <div className="flex flex-row md:py-6">
-        <h1>NEXT_SHOP</h1>
+      <div className={style.root}>
+        <Link href={`/`}>
+          <h1 className={style.logo}>NEXT_SHOP</h1>
+        </Link>
+        <nav className={style.nav}>
+          <Link href="/all">
+            <span className={style.link}>All</span>
+          </Link>
+          <Link href="/cloths">
+            <span className={style.link}>Cloths</span>
+          </Link>
+          <Link href="/accessories">
+            <span className={style.link}>Accessories</span>
+          </Link>
+          <Link href="/shoes">
+            <span className={style.link}>Shoes</span>
+          </Link>
+        </nav>
       </div>
     </Container>
   );
