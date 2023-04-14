2a2896f1c1b3c571e76b950d0e67c5c92f9aa914
Priti Roy <roypriti921@gmail.com>
2023-04-14 14:13:58 +0530

    added basic navbar component and implement it in layout

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,12 +1,13 @@
 import { FC, ReactNode } from "react";
 import style from "./Layout.module.css";
-import { Footer } from "@components/common";
+import { Footer, Navbar } from "@components/common";
 
 //Fixed 'Property 'children' does not exist on type {}' error by defining 'children' prop in the prop interface explicitly.
 
 const Layout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <div className={style.root}>
+      <Navbar />
       <main className="fit">{children}</main>
       <Footer />
     </div>
---
+++ components/common/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+import { Container } from "@components/ui";
+
+const Navbar: FC = () => {
+  return (
+    <Container>
+      <div className="flex flex-row md:py-6">
+        <h1>NEXT_SHOP</h1>
+      </div>
+    </Container>
+  );
+};
+
+export default Navbar;
