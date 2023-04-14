782d38f6f4d8f19eb6303640c27bd2929abc6ad0
Priti Roy <roypriti921@gmail.com>
2023-04-14 12:21:24 +0530

    added marquee scroll on marquee products

--- components/ui/Marquee/Marquee.tsx
+++ components/ui/Marquee/Marquee.tsx
@@ -1,10 +1,15 @@
 import React, { FC, ReactNode } from "react";
 import style from "./Marquee.module.css";
+import FastMarquee from "react-fast-marquee";
 
 const Marquee: FC<{ children: ReactNode[] }> = ({ children }) => {
   return (
     <div className={style.root}>
-      <div className={style.container}>{children}</div>
+      <div className={style.container}>
+        <FastMarquee gradient={false} speed={50}>
+          {children}
+        </FastMarquee>
+      </div>
     </div>
   );
 };
