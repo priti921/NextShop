fc084878a0a2e3390b0c7d451ccc147a3dbc47b2
Priti Roy <roypriti921@gmail.com>
2023-04-14 14:02:39 +0530

    created common component footer and implemented in layout && styled footer

---
+++ components/common/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from "react";
+
+const Footer: FC = () => {
+  return (
+    <footer className="bg-black pt-1">
+      <div className="container max-auto px-6">
+        <div className="mt-5 flex flex-col items-center">
+          <div className="py-6">
+            <p className="mb-b text-sm text-primary-2 font-bold">
+              &copy; {new Date().getFullYear()} Eincode
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from "react";
 import style from "./Layout.module.css";
+import { Footer } from "@components/common";
 
 //Fixed 'Property 'children' does not exist on type {}' error by defining 'children' prop in the prop interface explicitly.
 
@@ -7,6 +8,7 @@ const Layout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <div className={style.root}>
       <main className="fit">{children}</main>
+      <Footer />
     </div>
   );
 };
