7ec14dc846c71fd0a9de330ca39bec7582268020
Priti Roy <roypriti921@gmail.com>
2023-04-15 12:34:19 +0530

    added sidebar && CartSidebar to render in side sidebar ui component && rendered the sidebar inside layout compoennt

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,13 +1,17 @@
 import { FC, ReactNode } from "react";
 import style from "./Layout.module.css";
 import { Footer, Navbar } from "@components/common";
+import { Sidebar } from "@components/ui";
+import { CartSidebar } from "@components/cart";
 
 //Fixed 'Property 'children' does not exist on type {}' error by defining 'children' prop in the prop interface explicitly.
-
 const Layout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <div className={style.root}>
       <Navbar />
+      <Sidebar>
+        <CartSidebar />
+      </Sidebar>
       <main className="fit">{children}</main>
       <Footer />
     </div>
