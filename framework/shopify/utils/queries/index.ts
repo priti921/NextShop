fd0b9e70dca8f36900b6edf539094b57b78de059
Priti Roy <roypriti921@gmail.com>
2023-04-18 17:50:11 +0530

    fixed query file name

--- framework/shopify/utils/queries/index.ts
+++ framework/shopify/utils/queries/index.ts
@@ -1,2 +1,2 @@
 //index query exporter
-export { default as getAllProductsQuery } from "./get-all-products"; //gets import to index utils for further export
+export { default as getAllProductsQuery } from "./get-all-products-query"; //gets import to index utils for further export
