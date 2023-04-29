4cd130a3c2b44e35e9972d2e0864878208bb1d74
priti <roypriti921@gmail.com>
2023-04-29 16:30:40 +0530

    added slider than can slide on drag

--- components/product/ProductSlider/ProductSlider.tsx
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -6,19 +6,34 @@ import React, {
   isValidElement,
 } from "react";
 import style from "./ProductSlider.module.css";
+import { useKeenSlider } from "keen-slider/react";
 
 type Prop = {
   children: ReactNode;
 };
 
 const ProductSlider: FC<Prop> = ({ children }) => {
+  const [sliderRef, _] = useKeenSlider({
+    initial: 0,
+    loop: true,
+    slideChanged(s) {
+      console.log("changing to slide");
+    },
+  });
+
   return (
     <div className={style.root}>
-      <div className="keen-slider h-full transition-opacity">
+      <div
+        ref={sliderRef as any}
+        className="keen-slider h-full transition-opacity"
+      >
         {/* loops through each children and if the child is valid, clones the child to be able to pass className */}
         {Children.map(children, (child) => {
           if (isValidElement(child)) {
-            return cloneElement(child, { className: "keen-slider__slide" });
+            //copy the class name props if it exits plus keen-slider
+            return cloneElement(child, {
+              className: `${child.props.className ?? ""} keen-slider__slide`,
+            });
           }
           return child;
         })}
