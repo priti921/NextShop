896d01368cafc02079a8d62e6aa8aa29ab44b0fd
priti <roypriti921@gmail.com>
2023-04-30 00:14:58 +0530

    added helper function to get variant && moved choices type

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -5,19 +5,16 @@ import { Container, Button } from "@components/ui";
 import Image from "next/image";
 import { Product } from "@common/types/product";
 import { ProductSlider, Swatch } from "@components/product";
+import { Choices, getVariant } from "../helpers";
 
 interface Props {
   product: Product;
 }
 
-type AvailableChoices = "color" | "size" | string;
-
-type Choices = {
-  [P in AvailableChoices]: string;
-};
-
 const ProductView: FC<Props> = ({ product }) => {
   const [choices, setChoices] = useState<Choices>({});
+
+  const variant = getVariant(product, choices);
 
   return (
     <Container>
